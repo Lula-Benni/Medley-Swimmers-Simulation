18795bab219a98944b1206fa83d85859963b7251
Lulamile Plati <lulabenni45@gmail.com>
2024-09-06 05:22:01 +0200

    converted isOccupied to AtomicInteger and removed synchronization on the get method ,and it returns true now instead of false because spinning is fixed

--- src/medleySimulation/GridBlock.java
+++ src/medleySimulation/GridBlock.java
@@ -10,14 +10,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class GridBlock {
 	
-	private int isOccupied;
+	private final AtomicInteger isOccupied = new AtomicInteger(); //Use Atomic variables to Ensure thread safe access to isOccupied and isStart because they are accessed by multiple threads
 	
 	private final AtomicBoolean isStart = new AtomicBoolean();  //is this a starting block?
 	private int [] coords; // the coordinate of the block.
 	
 	GridBlock(boolean startBlock) throws InterruptedException {
 		isStart.set(startBlock);
-		isOccupied = -1;
+		isOccupied.set(-1);
 	}
 	
 	GridBlock(int x, int y, boolean startBlock) throws InterruptedException {
@@ -30,22 +30,21 @@ public class GridBlock {
 	public  int getY() {return coords[1];}
 	
 	//Get a block
-	// synchronized this method so that Threads get a block one at a time, so that they are always in different blocks
-	public synchronized boolean get(int threadID) throws InterruptedException {
-		if (isOccupied == threadID) return true; //thread Already in this block
-		if (isOccupied >= 0) return false; //space is occupied
-		isOccupied = threadID;  //set ID to thread that had block
-		return false;
+	public boolean get(int threadID) throws InterruptedException {
+		if (isOccupied.get() == threadID) return true; //thread Already in this block
+		if (isOccupied.get() >= 0) return false; //space is occupied
+        isOccupied.set(threadID);  //set ID to thread that had block
+		return true;
 	}
 
 	//release a block
 	public void release() {
-		isOccupied = -1;
+		isOccupied.set(-1);
 	}
 
 	//is a bloc already occupied?
 	public  boolean occupied() {
-		if(isOccupied == -1) return false;
+		if(isOccupied.get() == -1) return false;
 		return true;
 	}
 
